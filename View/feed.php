2c75078034fa38a5916bab0f2066cffb0a85f7d7
Alex Tamagnini <alex.tamagnini@studio.unibo.it>
2023-12-02 17:04:16 +0100

    bottom navbar

--- View/feed.php
+++ View/feed.php
@@ -141,7 +141,17 @@
 
         </div>
     </div>
-
+    <nav class="navbar bg-light fixed-bottom border-top border-2"> 
+        <div class="container">
+            <div class="d-flex justify-content-around w-100 fs-3">
+                <a href="#" class="text-center text-dark"><i class="fa fa-home"></i></a>
+                <a href="#" class="text-center text-muted"><i class="fa fa-search"></i></a>
+                <a href="#" class="text-center text-muted"><i class="fa fa-plus"></i></a>
+                <a href="#" class="text-center text-muted"><i class="fa fa-bell-o"></i></a>
+                <a href="#" class="text-center text-muted"><i class="fa fa-envelope-o"></i></a>
+            </div>
+        </div>
+    </nav>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
