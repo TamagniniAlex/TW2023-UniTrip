39603512b3181d1616bf4470c7f154cf58beebaa
Alex Tamagnini <alex.tamagnini@studio.unibo.it>
2023-12-13 12:46:24 +0100

    Fixed AddPost

--- Controller/addPostController.js
+++ Controller/addPostController.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
     var selectNations = $('#selectNations');
-    var selectCities = $('#selectCities');
-
-    // Prende le nazioni
     $.ajax({
         type: 'GET',
         url: '../Controller/getCountryController.php',
@@ -20,29 +17,8 @@ $(document).ready(function () {
             console.error('Errore nella richiesta AJAX:', status, error);
         }
     });
-
-    // Quando cambia la nazione selezionata
     selectNations.change(function () {
-        var selectedNation = $(this).val();
-
-        selectCities.empty();
-        selectCities.append('<option value="" disabled selected>Seleziona città</option>');
-
-        // Prende le città della nazione selezionata
-        $.ajax({
-            type: 'GET',
-            url: '../Controller/getCitiesController.php',
-            data: { nation: selectedNation },
-            dataType: 'json',
-            success: function (cities) {
-                cities.forEach(function (city) {
-                    selectCities.append('<option value="' + city.name + '">' + city.name + '</option>');
-                });
-            },
-            error: function (xhr, status, error) {
-                console.error('Errore nella richiesta AJAX:', status, error);
-            }
-        });
+        getCities(selectNations);
     });
 });
 
@@ -51,30 +27,59 @@ var stageCount = 1;
 function addStage() {
     var newStageHTML = `
         <div class="row mb-4">
-            <select id="selectCities${stageCount}" class="form-select" aria-label="Seleziona città di partenza">
-                <option selected disabled>Seleziona città di partenza</option>
-            </select>
+            <div class="col-8 p-0">
+                <select id="selectCities${stageCount}" class="form-select" aria-label="Seleziona città di partenza">
+                    <option selected disabled>Seleziona città di partenza</option>
+                </select>
+            </div>
+            <div class="col-4 p-0">
+                <input type="time" class="form-control" id="departureTime${stageCount}" name="departureTime">
+            </div>
         </div>
         <div class="row mb-4">
-            <input type="time" class="form-control" id="departureTime${stageCount}" name="departureTime">
-        </div>
-        <div class="row mb-4">
-            <select id="selectCities${stageCount + 1}" class="form-select" aria-label="Seleziona città di arrivo">
-                <option selected disabled>Seleziona città di arrivo</option>
-            </select>
-        </div>
-        <div class="row mb-4">
-            <input type="time" class="form-control" id="arrivalTime${stageCount}" name="arrivalTime">
+            <div class="col-8 p-0">
+                <select id="selectCities${stageCount + 1}" class="form-select" aria-label="Seleziona città di arrivo">
+                    <option selected disabled>Seleziona città di arrivo</option>
+                </select>
+            </div>
+            <div class="col-4 p-0">
+                <input type="time" class="form-control" id="arrivalTime${stageCount}" name="arrivalTime">
+            </div>
         </div>
         <hr class="row mb-4">
     `;
 
     var lastHr = document.querySelectorAll('hr')[document.querySelectorAll('hr').length - 1];
-
-    var wrapper = document.createElement('div');
-    wrapper.innerHTML = newStageHTML;
-
-    lastHr.parentNode.insertBefore(wrapper, lastHr.nextSibling);
-
+    lastHr.insertAdjacentHTML('afterend', newStageHTML);
     stageCount += 2;
+
+    var selectNations = $('#selectNations');
+    console.log(selectNations.val());
+    getCities(selectNations)
+}
+
+function getCities(selectNations) {
+    var selectedNation = $(selectNations).val();
+    for (var i = 1; i <= stageCount; i++) {
+        var selectCities = $('#selectCities' + i);
+        selectCities.empty();
+        selectCities.append('<option value="" disabled selected>Seleziona città</option>');
+    }
+    $.ajax({
+        type: 'GET',
+        url: '../Controller/getCitiesController.php',
+        data: { nation: selectedNation },
+        dataType: 'json',
+        success: function (cities) {
+            cities.forEach(function (city) {
+                for (var i = 1; i <= stageCount; i++) {
+                    var selectCities = $('#selectCities' + i);
+                    selectCities.append('<option value="' + city.name + '">' + city.name + '</option>');
+                }
+            });
+        },
+        error: function (xhr, status, error) {
+            console.error('Errore nella richiesta AJAX:', status, error);
+        }
+    });
 }
