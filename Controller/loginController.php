9c07e8f8ab7ab2f739ff235ef1a6a4acf609345e
Alex Tamagnini <alex.tamagnini@studio.unibo.it>
2024-01-08 15:31:50 +0100

    Fixed useless GET parameter

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -7,8 +7,7 @@ $nickname_mail = $_POST['nickname_mail'];
 $password = $_POST['password'];
 
 if ($login->login($nickname_mail, $password)) {
-    $_SESSION["nickname"] = $nickname_mail;
-    header('location: ../view/feed.html?follow=1&nickname=' . $nickname_mail);
+    header('location: ../view/feed.html?follow=1');
 } else {
     header('location:../view/index.html?error=1');
 }
